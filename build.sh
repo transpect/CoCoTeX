9e8aaf9d5a07ae6549f53e031b1fb49ce682f6d8
Marcus Hottenroth <marcus.hottenroth@le-tex.de>
2026-02-16 13:13:38 +0100

    Update build.sh to standard

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ export COCOTEX_SRC=$(dirname $(readlink -f $0))
 export CUR_PWD=`pwd`
 cd $COCOTEX_SRC/lib/ruby
 
-#bundle config --local without development
-#bundle config set --local frozen 'true'
-#bundle check > /dev/null || bundle install
-#bundle exec ruby frontend.rb $*
-ruby frontend.rb $*
+bundle config --local without development
+bundle config set --local frozen 'true'
+bundle check > /dev/null || bundle install
+bundle exec ruby frontend.rb $*
